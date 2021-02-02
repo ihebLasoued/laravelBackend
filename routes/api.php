3e177c1f25cf563f0c2c1731c97cacbfd421a355
lasoued.iheb@gmail.com <Iheb.lassoued@esprit.tn>
2021-02-02 10:39:14 +0100

    loginResgisteravecjwt

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+Route::post('login', 'UserController@login');
+Route::post('register', 'UserController@register');
+Route::group(['middleware' => 'auth.jwt'], function () {
+
 });
